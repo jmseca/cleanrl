1c3a0de81ef10bd35792f1892ce19bc4a50a1c9f
João Miguel Neves Fonseca <ist195749@sigma00.ist.utl.pt>
2024-02-13 20:02:37 +0000

    [Add] scripts to automate running multiple models

---
+++ automaterl/autorun.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+log_file="logautofile.log"
+names_file="model_names.txt"
+
+truncate -s 0 $log_file
+
+for ((i=0; i<=9; i++)); do
+    file="toRun/dqn_atari0${i}.py"
+
+    read model_name
+
+    echo $model_name > "model_names.txt"
+
+    echo "About to Run $file" >> "$log_file"
+    python3 "$file" < "model_names.txt"
+    echo "FINISHED $file" >> "$log_file"
+
+    read model_descr
+    #if cp -r "../runs/${model_name}/" toSend; then
+    echo "$model_descr" >> "runs/${model_name}/specs.txt"
+    echo "SPECS written ${model_name}" >> "$log_file"
+
+    echo "" >> "$log_file"
+    echo "" >> "$log_file"
+    #else
+    #    echo "Error: Failed to copy ${model_name}" >> "$log_file"
+    #fi
+
+done
